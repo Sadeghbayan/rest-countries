4c93a281a03f4bc66ccb5797521c8ec3c195dfd5
Soheil <liam@ramsalt.com>
2022-01-31 01:17:47 +0330

    basic markup for region/dropdown componenet

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ChevronDown } from "react-feather";
 
 const Dropdown: React.FC = () => {
-  return <div> Dropdown</div>;
+  return (
+    <label htmlFor="region" className="select-label">
+      <ChevronDown />
+      <select
+        id="region"
+        onChange={() => console.log("change")}
+        name="region"
+        value="Asia"
+        className="select-element"
+      >
+        <option disabled hidden className="select-optin" value="">
+          Placeholder
+        </option>
+      </select>
+    </label>
+  );
 };
 
 export default Dropdown;
