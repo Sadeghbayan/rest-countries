5e346810b77e8f4c05a5e93f5a508fac5abda3ef
Soheil <liam@ramsalt.com>
2022-01-30 04:11:45 +0330

    add toggle theme functionality for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
+
+type ITheme = "light" | "dark";
+
 function App() {
-  const toggleTheme = () => {};
+  const [defaultTheme, setDefaultTheme] = useState<ITheme>("light");
+  const toggleTheme = () => {
+    if (defaultTheme === "dark") {
+      setDefaultTheme("light");
+      document.querySelector("html")?.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      setDefaultTheme("dark");
+      document.querySelector("html")?.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
 
   return (
     <div className="App">
