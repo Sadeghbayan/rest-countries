2f174d77c34a934b58b131d5d8ee461afa9c460a
Soheil <liam@ramsalt.com>
2022-01-31 03:02:14 +0330

    add general type.d.ts and add country interface

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+type Country = {
+  flag: string;
+  alpha3Code: string;
+  name: string;
+  nativeName: string;
+  population: number;
+  region: string;
+  subregion: string;
+  capital: string;
+  topLevelDomain: string[];
+  currencies: Currency[];
+  languages: Languages[];
+  borders: string[];
+};
