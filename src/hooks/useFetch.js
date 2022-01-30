1d9a247c0fbae1d00f1b227953d68129501d4ed3
Soheil <liam@ramsalt.com>
2022-01-31 03:19:46 +0330

    add reusable hook for fetching data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+
+        setResponse(json);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { response, error };
+};
+
+export default useFetch;
