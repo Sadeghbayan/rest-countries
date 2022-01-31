bd981580e118839991887022e5188155b805d8fe
Soheil <liam@ramsalt.com>
2022-01-31 14:50:10 +0330

    add countries to country list
    styling for country wrapper

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import styles from "CountryList.module.scss";
+import styles from "./CountryList.module.scss";
+import CountryCard from "../CountryCard/CountryCard";
 
 interface ICountries {
-  countries: Country[];
+  countries: ICountry[];
 }
 
 const CountryList: React.FC<ICountries> = ({ countries }) => {
-  return <div>CountryList</div>;
+  return (
+    <div className={styles.countries}>
+      {countries.map((country) => (
+        <CountryCard country={country} key={country.name} />
+      ))}
+    </div>
+  );
 };
 
 export default CountryList;
