76d772ebe5c4612fafd0832714f0a84cbaf1b269
Soheil <liam@ramsalt.com>
2022-01-31 14:50:59 +0330

    implement country card and styling

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styles from "./CountryCard.module.scss";
+import Img from "react-cool-img";
 
-const CountryCard: React.FC = () => {
-  return <div>Country Card</div>;
+interface ICountryCard {
+  country: ICountry;
+}
+
+const CountryCard: React.FC<ICountryCard> = ({ country }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles["card-image"]}>
+        <Img src={country.flag} alt={`Flag of ${country.name}`} />
+      </div>
+      <div className={styles["card-info"]}>
+        <div className={styles["card-name"]}>
+          <h3>{country.name}</h3>
+        </div>
+        <div className={styles["card-label"]}>
+          <span>Population: </span> {country.population.toLocaleString("en-US")}
+        </div>
+        <div className={styles["card-label"]}>
+          <span>Region: </span> {country.region}
+        </div>
+        <div className={styles["card-label"]}>
+          <span>Capital: </span> {country.capital}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CountryCard;
